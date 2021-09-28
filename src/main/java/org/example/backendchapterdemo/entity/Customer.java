e78aa7084278a205a7e775f6b41ace11c36b2ecc
ayehia <a.yehia@boutiqaat.com>
2021-09-28 16:26:40 +0300

    Removed unnecessary annotation

--- src/main/java/org/example/backendchapterdemo/entity/Customer.java
+++ src/main/java/org/example/backendchapterdemo/entity/Customer.java
@@ -1,6 +1,5 @@
 package org.example.backendchapterdemo.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -23,6 +22,5 @@ public class Customer {
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @MapsId
     @JoinColumn(name = "user_id", columnDefinition = "uuid")
-    @JsonIgnore
     private User user;
 }
